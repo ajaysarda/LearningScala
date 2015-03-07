f6d1b995af373419d1701cc8a7aba8b1bbd45cc0
unknown <ajasarda@US-SEA-R8H4FHE.ant.amazon.com>
2015-03-06 16:12:37 -0800

    Initial commit

---
+++ src/main/scala/ExampleApp.scala
@@ -0,0 +1,21 @@
+/**
+ * Created by ajasarda on 3/6/2015.
+ */
+package example
+
+/*
+Approach 1:
+object ExampleApp extends App {
+  println("Hello World!")
+}
+*/
+
+/**
+  * Approach 2:
+  */
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello World!")
+  }
+}
+
