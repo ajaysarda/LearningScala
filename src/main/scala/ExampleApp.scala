d436013e4148c3fe3a3a56ecf050a9fdeda54e2c
unknown <ajasarda@US-SEA-R8H4FHE.ant.amazon.com>
2015-03-06 16:12:37 -0800

    Initial commit

---
+++ src/main/scala/ExampleApp.scala
@@ -0,0 +1,22 @@
+/**
+ * Created by ajasarda on 3/6/2015.
+ */
+package example
+
+/*
+Approach 1:
+object ExampleApp extends App {
+  println("Hello World!")
+}
+*/
+
+/**
+  * Approach 2:
+  *  Like Java, you can also define the entry point method "main" and write print statement inside it
+  */
+object ExampleApp {
+  def main(args: Array[String]) {
+    println("Hello World!")
+  }
+}
+
